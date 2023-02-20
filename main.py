99ca832d370299239c8cdc1c207142421fc01a70
Pimouss75 <124018242+Pimouss75@users.noreply.github.com>
2023-02-20 18:12:38 +0100

    shap a faire en commentaire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import Needed Libraries
 import joblib
+import dill
 import uvicorn
 import numpy as np
 import pandas as pd
@@ -12,9 +13,9 @@ from fastapi import FastAPI
 app = FastAPI(title='Admissibilité a un pret', version='1.0',
               description='model optimise pour la prediction')
 
-# Initialize model artifacte files. This will be loaded at the start of FastAPI model server.
+# Initialize model artifact files. This will be loaded at the start of FastAPI model server.
 model = joblib.load("model.pkl")
-# explainer = dill.load(open("shap_explainer.dill","rb"))
+explainer = dill.load(open("shap_explainer.dill","rb"))
 df = pd.read_csv("df_test.csv")
 
 
@@ -43,17 +44,27 @@ def read_home():
 
 # ML API endpoint for making prediction aganist the request received from client
 @app.post("/predict/{client_id}")
-def predict(client_id:int):
-    data = df[df["SK_ID_CURR"] == client_id].drop(['TARGET', 'SK_ID_CURR'], axis=1).iloc[0].to_dict()
-    prediction = model.predict_proba(pd.DataFrame(columns=list(data.keys()), data=[list(data.values())]))
-    if prediction[0][1] > 0.54:
-        prediction_result = "Client susceptible d'avoir des difficultés à rembourser un prêt."
-    else:
-        prediction_result = "Client à faible risque d'être en défaut de paiement si un prêt lui est consenti."
+def predict(client_id:str):
+    df.SK_ID_CURR = df.SK_ID_CURR.astype('str')
+    client_id = str(client_id)
+    data = df[df["SK_ID_CURR"] == client_id].drop(['TARGET', 'SK_ID_CURR'], axis=1)
+    if len(data) == 0 :
+        prediction_result = f"Client {client_id} non référencé, prediction impossible."
 
-    # get Shap values from preprocessed data
-#    shap_values = explainer.shap_values(list(data.values()))
-#    shap.force_plot(explainer.expected_value[0], shap_values[0], feature_names=data.keys())
+    else:
+#        data = data.iloc[0].to_dict()
+#        prediction = model.predict_proba(pd.DataFrame(columns=list(data.keys()), data=[list(data.values())]))
+        prediction = model.predict_proba(data)
+
+        if prediction[0][1] > 0.54:
+            prediction_result = "Client susceptible d'avoir des difficultés à rembourser un prêt."
+#            shap_values = explainer.shap_values(data)
+#            shap = shap.force_plot(explainer.expected_value[0], shap_values[0], feature_names=data.columns)
+        else:
+            prediction_result = "Client à faible risque d'être en défaut de paiement si un prêt lui est consenti."
+#            shap_values = explainer.shap_values(data)
+#            shap = shap.force_plot(explainer.expected_value[0], shap_values[1], feature_names=data.columns)
+
 
     return prediction_result
 
