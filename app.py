68c8ac634221761e27c9744f37f3ab620c2b57de
Pimouss75 <124018242+Pimouss75@users.noreply.github.com>
2023-02-26 19:17:29 +0100

    shap

--- app.py
+++ app.py
@@ -1,31 +1,56 @@
 import streamlit
-import requests as re
+import requests
 import json
 import pandas as pd
+import dill
+import shap
+import streamlit_shap
 
-# df = pd.read_csv("df_test.csv")
+streamlit.set_option('deprecation.showPyplotGlobalUse', False)
+url_aws = "http://127.0.0.1:8000"
 
 def run():
     streamlit.title("Prediction d'admissibilité a un pret")
     client_id = streamlit.text_input("Reference client SK_ID_CURR : ")
 
-
     data_pred = {
         "SK_ID_CURR":client_id,
     }
 
-
     if streamlit.button("Predict"):
         streamlit.info(data_pred)
-        #response = requests.post("http://0.0.0.0:8000/predict", data=json.dumps(data_pred, default=str))
-        #response = requests.post("http://127.0.0.1:8000/predict", json=data_pred )
-        #prediction = response.text
-        #streamlit.success(f"The prediction from model: {prediction}")
-#        response = re.post("http://127.0.0.1:8000/predict", data=json.dumps(data_pred, default=str))
-        response = re.post("http://127.0.0.1:8000/predict/" + str(client_id), data=json.dumps(data_pred, default=str))
-#        response = re.post("http://0.0.0.0:8000/predict/" + str(client_id), data=json.dumps(data_pred, default=str))
+
+        response = requests.post(f"{url_aws}/predict/" + str(client_id), data=json.dumps(data_pred, default=str))
         prediction = response.text
         streamlit.success(f"La prediction pour ce client est: \n {prediction}")
+
+    if streamlit.sidebar.button("Features importance"):
+
+        # Chargement du de l'explainer
+        explainer = dill.load(open("shap_explainer.dill", "rb"))
+
+        response = requests.get(f"{url_aws}/client/" + str(client_id),verify=False)
+        if response.status_code == 200:
+            X_test = response.json()["data"]
+#            X_test = pd.DataFrame(X_test)
+            X_test = pd.DataFrame(X_test, index=pd.RangeIndex(len(X_test)))
+
+            shap_values = explainer.shap_values(X_test)
+
+
+            streamlit.subheader("Interprétabilité shap du client")
+            # Affichage du graphique
+
+            streamlit_shap.st_shap(shap.force_plot(explainer.expected_value[0],
+                                                   shap_values[0][1],
+                                                   feature_names=X_test.columns))
+
+
+            shap.plots._waterfall.waterfall_legacy(explainer.expected_value[0],
+                                                   shap_values[0][1],
+                                                   feature_names=X_test.columns,
+                                                   max_display=10)
+            streamlit.pyplot()
 
 
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Import Needed Libraries
+# Importation des bibliothèques nécessaires
 import joblib
 import dill
 import uvicorn
@@ -6,71 +6,67 @@ import numpy as np
 import pandas as pd
 from pydantic import BaseModel
 
-# FastAPI libray
-from fastapi import FastAPI
+# Importation de la bibliothèque FastAPI
+from fastapi import FastAPI, HTTPException
 
 # Initiate app instance
-app = FastAPI(title='Admissibilité a un pret', version='1.0',
-              description='model optimise pour la prediction')
+app = FastAPI(title='Admissibilité à un prêt', version='1.0',
+              description='Modèle optimisé pour la prédiction')
 
-# Initialize model artifact files. This will be loaded at the start of FastAPI model server.
+# Chargement des fichiers d'artefacts de modèle au lancement du serveur FastAPI
 model = joblib.load("model.pkl")
-explainer = dill.load(open("shap_explainer.dill","rb"))
 df = pd.read_csv("df_test.csv")
+df_selected = df.drop(['TARGET', 'SK_ID_CURR'], axis=1)
 
-
-
-# This struture will be used for Json validation.
-# With just that Python type declaration, FastAPI will perform below operations on the request data
-## 1) Read the body of the request as JSON.
-## 2) Convert the corresponding types (if needed).
-## 3) Validate the data.If the data is invalid, it will return a nice and clear error,
-##    indicating exactly where and what was the incorrect data.
-
+# Définition d'une classe Pydantic pour valider la requête
 class Data(BaseModel):
     SK_ID_CURR: str
 
 
 
-# Api root or home endpoint
+# Point de terminaison racine ou de base
 @app.get('/')
 @app.get('/home')
 def read_home():
     """
-     Home endpoint which can be used to test the availability of the application.
+     Endpoint de base qui peut être utilisé pour tester la disponibilité de l'application.
      """
-    return {'message': 'System is healthy'}
+    return {'message': 'Le système est opérationnel'}
 
-
-# ML API endpoint for making prediction aganist the request received from client
+# Point de terminaison pour effectuer des prédictions en fonction de l'ID du client
 @app.post("/predict/{client_id}")
-def predict(client_id:str):
+def predict(client_id: str):
     df.SK_ID_CURR = df.SK_ID_CURR.astype('str')
     client_id = str(client_id)
     data = df[df["SK_ID_CURR"] == client_id].drop(['TARGET', 'SK_ID_CURR'], axis=1)
-    if len(data) == 0 :
-        prediction_result = f"Client {client_id} non référencé, prediction impossible."
-
+    if len(data) == 0:
+        raise HTTPException(status_code=404, detail=f"Client {client_id} non référencé, prédiction impossible.")
     else:
-#        data = data.iloc[0].to_dict()
-#        prediction = model.predict_proba(pd.DataFrame(columns=list(data.keys()), data=[list(data.values())]))
+        # Seuil optimal
+        seuil = 0.54
         prediction = model.predict_proba(data)
-
-        if prediction[0][1] > 0.54:
+        if prediction[0][1] > seuil:
             prediction_result = "Client susceptible d'avoir des difficultés à rembourser un prêt."
-#            shap_values = explainer.shap_values(data)
-#            shap = shap.force_plot(explainer.expected_value[0], shap_values[0], feature_names=data.columns)
         else:
             prediction_result = "Client à faible risque d'être en défaut de paiement si un prêt lui est consenti."
-#            shap_values = explainer.shap_values(data)
-#            shap = shap.force_plot(explainer.expected_value[0], shap_values[1], feature_names=data.columns)
+        return prediction_result
 
 
-    return prediction_result
+# Point de terminaison pour obtenir les données d'un client en fonction de son ID
+@app.get("/client/{client_id}")
+def get_client(client_id: str) -> dict:
+    df.SK_ID_CURR = df.SK_ID_CURR.astype('str')
+    client_id = str(client_id)
+    data = df[df["SK_ID_CURR"] == client_id].drop(['TARGET', 'SK_ID_CURR'], axis=1)
+    if len(data) == 0:
+        raise HTTPException(status_code=404, detail=f"Client {client_id} non référencé.")
+    else:
+        data = data.to_dict(orient="records")[0]
+        return {"data": data}
 
 
 if __name__ == '__main__':
-#    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
-    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+#    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
 
 # lancer l'api: uvicorn main:app --reload
